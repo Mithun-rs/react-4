c4bf26cd1553fc7ff86da99cd69a586fbadcb567
Mithun RS <mithunsivanantham183@gmail.com>
2025-02-18 09:18:42 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
   return (
     <nav style={{ padding: "10px", background: "blue" }}>
       <ul style={{ display: "flex", listStyle: "none", gap: "15px" }}>
-        <li><Link to="/page1" style={{ color: "#fff" }}>Page 1</Link></li>
-        <li><Link to="/page2" style={{ color: "#fff" }}>Page 2</Link></li>
-        <li><Link to="/page3" style={{ color: "#fff" }}>Page 3</Link></li>
-        <li><Link to="/page4" style={{ color: "#fff" }}>Page 4</Link></li>
-        <li><Link to="/page5" style={{ color: "#fff" }}>Page 5</Link></li>
+        <li><Link to="/react-4/page1" style={{ color: "#fff" }}>Page 1</Link></li>
+        <li><Link to="/react-4/page2" style={{ color: "#fff" }}>Page 2</Link></li>
+        <li><Link to="/react-4/page3" style={{ color: "#fff" }}>Page 3</Link></li>
+        <li><Link to="/react-4/page4" style={{ color: "#fff" }}>Page 4</Link></li>
+        <li><Link to="/react-4/page5" style={{ color: "#fff" }}>Page 5</Link></li>
       </ul>
     </nav>
   );
